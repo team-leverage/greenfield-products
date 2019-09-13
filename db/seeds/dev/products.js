83cee1e0125159717f2c3cb4a19a7a00948d344a
han-yan-ds <hanprogramming@gmail.com>
2019-09-13 14:14:56 -0600

    Added error handling to products seeding to identify unique-related errors and other errors

--- db/seeds/dev/products.js
+++ db/seeds/dev/products.js
@@ -1,14 +1,15 @@
-
-const readLine = require('readline');
+const lineByLine = require('line-by-line');
 const fs = require('fs');
-const url = `./data/product.csv`;
+const url = `./data/babyproduct.csv`;
+
+var isCheckUniqueError = function (err) {
+  return err.routine.includes('unique');
+}
 
 var insertProduct = function(knex) {
   return new Promise(function () {
     let isFirstLine = true, thisLine = 1;
-    let rl = readLine.createInterface({
-      input: fs.createReadStream(url)
-    });
+    let rl = new lineByLine(url);
     rl.on('line', function(line) {
       if (isFirstLine) {
         console.log('Starting To Load CSV into Database');
@@ -16,14 +17,18 @@ var insertProduct = function(knex) {
         return;
       }
       let [id, name, slogan, description, category, default_price] = JSON.parse(`[${line}]`);
-      if (thisLine % 50000 === 0) { console.log(`Finished ${thisLine} items!`); }
+      if (thisLine % 25000 === 0) { console.log(`Finished ${thisLine} items!`); }
       thisLine ++;
       return Promise.all([
         knex('categories').insert({category_name: category}, 'category_id')
-          .catch(() => { // if category duplicate error, select from existing
-            return knex('categories').where('category_name', category).select('category_id').then((selectObj) => {
-              return selectObj[0]['category_id'];
-            })
+          .catch((err) => {
+            if (isCheckUniqueError(err)) { // if category duplicate error, select from existing
+              return knex('categories').where('category_name', category).select('category_id').then((selectObj) => {
+                return selectObj[0]['category_id'];
+              })
+            } else {  // if the error is another error
+              console.log(`PROBLEM LINE FOR CATEGORIES ${thisLine}: ${line}`)
+            }
           })
           .then((category_id) => {
             // console.log(`Inserted ${name} with category ${category_id}`);
@@ -36,12 +41,18 @@ var insertProduct = function(knex) {
               default_price: Number(default_price)
             })
           })
-          .catch((err) => {})
+          .catch((err) => {
+            if (isCheckUniqueError(err)) { // if the error is related to unique contraints for products
+              console.log(`DUPLICATE PRODUCT ${name}`);
+            } else{
+              console.log(`PROBLEM LINE FOR PRODUCTS ${thisLine}: ${line}`);
+            }
+          })
       ])
       // .then(() => id)
       .catch(((err) => {console.log(err)}))
     });
-    rl.on('close', () => console.log('DONE!!'));
+    rl.on('end', () => console.log('DONE!!'));
   })
 }
 
