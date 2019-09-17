9259fe0ba93838ba9d22b78e4648f3a7a993f7bf
han-yan-ds <hanprogramming@gmail.com>
2019-09-17 14:34:09 -0600

    Finished getProductInfo query

--- server/queries.js
+++ server/queries.js
@@ -2,8 +2,6 @@ var {staging} = require('../knexfile');
 var knex = require('knex')(staging);
 
 exports.getProductList = function (num, cb) {
-  //write query for top num # of entries in product list
-  //full join categories, products
   let productFields = [
     'id',
     'name',
@@ -13,13 +11,35 @@ exports.getProductList = function (num, cb) {
     'default_price',
   ];
   knex.select(...productFields).from('products').fullOuterJoin('categories', 'categories.category_id', 'products.category_id').limit(num)
-  .then((err, result) => {
-    cb(err, result)
+  .then((result) => {
+    cb(result)
   });
 }
 
+exports.getProductFeatures = function (productId, cb) {
+  let featureFields = ['feature', 'value'];
+  knex.select(...featureFields).from('product_feature_join').where({product_id: productId})
+    .fullOuterJoin('feature_values', 'product_feature_join.feature_value_id', 'feature_values.feature_value_id')
+    .fullOuterJoin('feature_names', 'feature_values.feature_name_id', 'feature_names.feature_name_id')
+  .then((result) => {
+    cb(result)
+  })
+}
+
 exports.getProductInfo = function (productId, cb) {
-  
+  let productFields = [
+    'id',
+    'name',
+    'slogan',
+    'description',
+    'category',
+    'default_price',
+  ];
+  knex.select(...productFields).from('products').where({id: productId})
+    .fullOuterJoin('categories', 'categories.category_id', 'products.category_id')
+  .then((result) => {
+    cb(result)
+  })
 }
 
 exports.getStyles = function (productId, cb) {
