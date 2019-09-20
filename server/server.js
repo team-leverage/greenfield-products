b317c334530264b134d92361bdf68aa524565b11
han-yan-ds <hanprogramming@gmail.com>
2019-09-19 19:15:31 -0600

    Added newrelic

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('newrelic');
 
 const {
   getProductList,
